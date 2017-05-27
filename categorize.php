2b3a2fe697733ca5abf781f34fd20fba869167ef
Suttipat Sukwattanakorn <suttipat.new@gmail.com>
2017-05-27 12:44:20 +0700

    finish login success and fail

--- categorize.php
+++ categorize.php
@@ -1,8 +1,23 @@
 <!DOCTYPE html>
+<?php
+	error_reporting(E_ERROR | E_PARSE);
+	define('DB_SERVER', 'localhost');
+	define('DB_USERNAME', $_POST['username']);
+	define('DB_PASSWORD', $_POST['password']);
+	define('DB_DATABASE', 'test');
+	define('HOMEPAGE', 'http://localhost/webstock');
+	// $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+	$db = new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+	if($db->connect_error) {
+		header("Location: ".HOMEPAGE."/?login_failed=1");
+		die();
+	}
+// show tables;
+?>
 <html>
 <head>
 	<title></title>
-	<meta charset="utf-8" /> 
+	<meta charset="utf-8" />
 	<link rel="stylesheet" type="text/css" href="style.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 	<script src="https://code.jquery.com/jquery-2.2.0.min.js"></script>
--- index.php
+++ index.php
@@ -7,12 +7,17 @@
     </head>
     <body>
         <div class="login">
-           <form class="" action="login.php" method="post">
+           <form class="" action="categorize.php" method="post">
             <ul class='login'>
                     <li>Username: <input type='text' name='username'></li>
                     <li>Password: <input type='password' name='password'></li>
             </ul>
             <br><input type="submit" value="Submit">
+            <?php
+               if(isset($_GET['login_failed']) && $_GET['login_failed'] == 1) {
+                  echo '<p style="color: red;">Wrong username or password.</p>';
+               }
+            ?>
             </form>
         </div>
     </body>

937b01d2ae1178b88db577564f8995c74f6528ac
Suttipat Sukwattanakorn <suttipat.new@gmail.com>
2017-05-27 12:21:34 +0700

    add login page

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Login Page</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+        <div class="login">
+           <form class="" action="login.php" method="post">
+            <ul class='login'>
+                    <li>Username: <input type='text' name='username'></li>
+                    <li>Password: <input type='password' name='password'></li>
+            </ul>
+            <br><input type="submit" value="Submit">
+            </form>
+        </div>
+    </body>
+</html>

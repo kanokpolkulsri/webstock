e6c19e63b8abebf1ce573442c2c930e657a5a7df
SuttipatNew <suttipat.new@gmail.com>
2017-06-01 03:21:05 +0700

    add test data

--- projects.sql
+++ projects.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 31, 2017 at 10:11 PM
+-- Generation Time: May 31, 2017 at 10:20 PM
 -- Server version: 10.1.21-MariaDB
 -- PHP Version: 5.6.30
 
@@ -37,7 +37,31 @@ CREATE TABLE `project_map` (
 --
 
 INSERT INTO `project_map` (`ProjWBS`, `ProjName`, `LastUpdate`) VALUES
-('EH123456789', 'ลองดู', '2017-05-31 13:03:08');
+('1234', 'โครงการทดลอง', '2017-05-31 13:12:49');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `Username` varchar(50) NOT NULL,
+  `Name` varchar(400) NOT NULL,
+  `Phone` varchar(20) NOT NULL,
+  `Company` varchar(400) NOT NULL,
+  `Position` varchar(400) NOT NULL,
+  `State` varchar(10) NOT NULL,
+  `LastUpdate` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`Username`, `Name`, `Phone`, `Company`, `Position`, `State`, `LastUpdate`) VALUES
+('user1', 'user1', '-', '-', '-', 'admin', '2017-06-01 00:00:00'),
+('user2', 'user2', '-', '-', '-', 'user', '2017-05-31 13:06:02');
 
 --
 -- Indexes for dumped tables
@@ -48,6 +72,12 @@ INSERT INTO `project_map` (`ProjWBS`, `ProjName`, `LastUpdate`) VALUES
 --
 ALTER TABLE `project_map`
   ADD PRIMARY KEY (`ProjWBS`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`Username`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;

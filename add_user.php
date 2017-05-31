d423a7f7d9f451dbb2e5195f3bac1f490d5ff955
SuttipatNew <suttipat.new@gmail.com>
2017-05-31 22:26:53 +0700

    prepare to create add_user.php

---
+++ add_user.php
@@ -0,0 +1,30 @@
+<?php
+   session_start();
+	error_reporting(E_ERROR | E_PARSE);
+	date_default_timezone_set("Asia/Bangkok");
+   define('DB_SERVER', 'localhost');
+   define('DB_USERNAME', $_SESSION['username']);
+   define('DB_PASSWORD', $_SESSION['password']);
+   // define('DB_USERNAME', 'user1');
+   // define('DB_PASSWORD', '1q2w3e4r');
+   define('DB_DATABASE', 'projects');
+   define('HOMEPAGE', '.');
+   // $conn = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+   $conn = new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD, DB_DATABASE);
+   mysqli_set_charset($conn, "utf8");
+   $sql = 'SELECT * FROM project_map ORDER BY LastUpdate';
+   $result = $conn->query($sql);
+
+   // $result = $conn->query("SHOW DATABASES;");
+   if ($result->num_rows > 0) {
+      $output = '[';
+      while($row = $result->fetch_assoc()) {
+         $output .= '{ "ProjWBS" : "' . $row['ProjWBS'] . '", "ProjName" : "' . $row['ProjName']. '"}, ';
+      }
+      $output = substr($output, 0, strlen($output)-2);
+      $output .= ']';
+      echo $output;
+   } else {
+       echo "0";
+   }
+?>

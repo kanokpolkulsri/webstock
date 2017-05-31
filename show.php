713bbbdd20810136b0a76b9f785ad2ffae87585f
SuttipatNew <suttipat.new@gmail.com>
2017-05-31 05:38:49 -0700

    fix show.php

--- show.php
+++ show.php
@@ -1,22 +1,4 @@
 <!DOCTYPE html>
-<?php
-   session_start();
-   error_reporting(E_ERROR | E_PARSE);
-   date_default_timezone_set("Asia/Bangkok");
-   define('DB_SERVER', 'localhost');
-   define('DB_USERNAME', $_SESSION['username']);
-   define('DB_PASSWORD', $_SESSION['password']);
-   // define('DB_USERNAME', 'user1');
-   // define('DB_PASSWORD', '1q2w3e4r');
-   define('DB_DATABASE', 'projects');
-   define('HOMEPAGE', '.');
-   $conn = new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD, DB_DATABASE);
-   if($conn->connect_error) {
-    //   header("Location: ".HOMEPAGE."/?login_failed=1");
-      die('connection error');
-   }
-   mysqli_set_charset($conn, "utf8");
-?>
 <html>
 <head>
 	<title></title>
@@ -27,55 +9,217 @@
 	<script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
 </head>
 <body class="body-show">
-	<p class="title-cate">WELCOME</p>
+	<div class="show-title-Proj">
+		<p class="show-title-Proj-name">NAME</p>
+		<p class="show-title-Proj-WBS">WBS</p>
+	</div>
+	<div>
+		<button class="show-btn-inv">สินค้าคงคลัง</button>
+		<button class="show-btn-rec">ประวัติการรับสินค้า</button>
+		<button class="show-btn-out">ประวัติการเบิกสินค้า</button>
+	</div>
+	<div class="show-all-data-inv">
+	inv
 		<div class="wrapper">
-			<div class="table-cpe">
+			<div class="table-inv">
+				<div class="row header green">
+					<div class="cell">รหัสสินค้า</div>
+					<div class="cell">ชื่อสินค้า</div>
+					<div class="cell">จำนวน</div>
+					<div class="cell">หน่วย</div>
+					<div class="cell">ราคาต่อหน่วย</div>
+					<div class="cell">ราคารวม</div>
+					<div class="cell">ที่เก็บสินค้า</div>
+	    		</div>
+			</div>
+		</div>
+		<input type="file" name="file" id="file-inv" style="display: block;">
+	</div>
+
+	<div class="show-all-history-rec">
+	rec
+		<div class="wrapper">
+			<div class="table-rec">
 				<div class="row header green">
 					<div class="cell">วันที่</div>
+					<div class="cell">รหัสสินค้า</div>
+					<div class="cell">ชื่อสินค้า</div>
 					<div class="cell">จำนวน</div>
 					<div class="cell">หน่วย</div>
 					<div class="cell">ราคาต่อหน่วย</div>
 					<div class="cell">ราคารวม</div>
 					<div class="cell">ผู้รับสินค้า</div>
-					<div class="cell">ที่เก็บ</div>
-	    		</div>
-	    		<div class="row">
-					<div class="cell">01/01/2015</div>
-					<div class="cell">10</div>
-					<div class="cell">ชิ้น</div>
-					<div class="cell">5</div>
-					<div class="cell">50</div>
-					<div class="cell">admin</div>
-					<div class="cell">storage1</div>
-	    		</div>
-	    		<div class="row">
-					<div class="cell">02/01/2015</div>
-					<div class="cell">120</div>
-					<div class="cell">ชิ้น</div>
-					<div class="cell">10</div>
-					<div class="cell">1200</div>
-					<div class="cell">admin</div>
-					<div class="cell">storage1</div>
+					<div class="cell">รับจากบริษัท</div>
+					<div class="cell">ที่เก็บสินค้า</div>
 	    		</div>
 			</div>
 		</div>
-	<input type="file" name="file" id="file-cpe" style="display: block;">
+		<input type="file" name="file" id="file-rec" style="display: block;">
+	</div>
+
+	<div class="show-all-history-out">
+	out
+		<div class="wrapper">
+			<div class="table-out">
+				<div class="row header green">
+					<div class="cell">วันที่</div>
+					<div class="cell">รหัสสินค้า</div>
+					<div class="cell">ชื่อสินค้า</div>
+					<div class="cell">จำนวน</div>
+					<div class="cell">หน่วย</div>
+					<div class="cell">ราคาต่อหน่วย</div>
+					<div class="cell">ราคารวม</div>
+					<div class="cell">ผู้เบิกสินค้า</div>
+					<div class="cell">บริษัทผู้เบิก</div>
+					<div class="cell">ผู้ดูแลระบบ</div>
+					<div class="cell">ที่เก็บสินค้า</div>
+	    		</div>
+			</div>
+		</div>
+		<input type="file" name="file" id="file-out" style="display: block;">
+	</div>
+
+	<div class="show-delete-this-proj">
+		<button class="show-btn-delete">Delete this project</button><br>
+		<div class="show-delete-confirm">
+		Project Name : <input class="show-input-del-name"/><br>
+		WBS No. : <input class="show-input-del-WBS"/><br>
+		<button class="show-btn-confirm-del">ยืนยันการลบ</button>
+		</div>
+
+	</div>
+
 	<script type="text/javascript">
-		var tablecpe = $('.table-cpe');
-		document.getElementById('file-cpe').onchange = function(){
+		/*
+			ลบโครงการ
+		*/
+		var buttonDel = $('.show-btn-delete'); //ปุ่มเปิดว่าจะกรอกชื่อและwbs
+		var buttonDelConfirm = $('.show-btn-confirm-del'); //ปุ่มที่ยืนยันว่าจะลบข้อมูลแล้ว
+		var inputDelName = $('.show-input-del-name');
+		var inputDelWBS = $('.show-input-del-WBS');
+		var deleteConfirm = $('.show-delete-confirm'); //div ที่คลุมส่วนกรอกข้อมูล
+		var checkButtonDelClicked = false;
+		deleteConfirm.hide();
+
+		//กดปุ่มเลือกว่าจะลบ
+		buttonDel.click(function(){
+			if(!checkButtonDelClicked){
+				checkButtonDelClicked = true;
+				deleteConfirm.show();
+			}else{
+				checkButtonDelClicked = false;
+				deleteConfirm.hide();
+			}
+		});
+
+		//กดปุ่มยืนยัน ก็คือจะเอาค่าจาก input ไปเทียบกับ database
+		buttonDelConfirm.click(function(){
+			//inputDelName.val() //ค่าที่จะเอาไปเช็คว่าชื่อตรงกับโปรเจคไหม
+			//inputDelWBS.val() //ค่าที่จะเอาไปเช็คว่าwbsตรงกับโปรเจคไหม
+			//ถ้าตรงก็ทำการลบ
+		});
+
+
+		/*
+			เลือกว่าจะดูอันไหน
+		*/
+		var buttonInv = $('.show-btn-inv');
+		var buttonRec = $('.show-btn-rec');
+		var buttonOut = $('.show-btn-out');
+		var divInv = $('.show-all-data-inv');
+		var divRec = $('.show-all-history-rec');
+		var divOut = $('.show-all-history-out');
+		divRec.hide();
+		divOut.hide();
+		divInv.show();
+		function outall(){
+			buttonInv.removeClass('show-btn-active');
+			buttonRec.removeClass('show-btn-active');
+			buttonOut.removeClass('show-btn-active');
+			divInv.hide();
+			divRec.hide();
+			divOut.hide();
+		}
+		buttonInv.click(function(){
+			outall();
+			buttonInv.addClass('show-btn-active');
+			divInv.show();
+		});
+		buttonRec.click(function(){
+			outall();
+			buttonRec.addClass('show-btn-active');
+			divRec.show();
+		});
+		buttonOut.click(function(){
+			outall();
+			buttonOut.addClass('show-btn-active');
+			divOut.show();
+		});
+
+
+
+		/*
+			ส่วนของการเพิ่มไฟล์สินค้าคงคลัง
+		*/
+		var tableinv = $('.table-inv');
+		document.getElementById('file-inv').onchange = function(){
 			var file = this.files[0];
 			var reader = new FileReader();
 			reader.onload = function(progressEvent){
 			var lines = this.result.split('\n');
 			for(var line = 0; line < lines.length; line++){
 				var check = lines[line].split(",");
-				tablecpe.append('<div class="row"><div class="cell">'+check[0]+'</div><div class="cell">'+check[1]+'</div><div class="cell">'+check[2]+'</div><div class="cell">'+check[3]+'</div><div class="cell">'+check[4]+'</div></div>');
+
+				// tableinv.append('<div class="row"><div class="cell">'+check[0]+'</div><div class="cell">'+check[1]+'</div><div class="cell">'+check[2]+'</div><div class="cell">'+check[3]+'</div><div class="cell">'+check[4]+'</div></div>');
+
 				}
-				console.log(check[1]);
-				console.log(check[2]);
+			};
+			reader.readAsText(file);
+		};
+
+
+
+		/*
+			ส่วนของการเพิ่มไฟล์ประวัติการรับสินค้า
+		*/
+		var tablerec = $('.table-rec');
+		document.getElementById('file-rec').onchange = function(){
+			var file = this.files[0];
+			var reader = new FileReader();
+			reader.onload = function(progressEvent){
+			var lines = this.result.split('\n');
+			for(var line = 0; line < lines.length; line++){
+				var check = lines[line].split(",");
+
+				// tablerec.append('<div class="row"><div class="cell">'+check[0]+'</div><div class="cell">'+check[1]+'</div><div class="cell">'+check[2]+'</div><div class="cell">'+check[3]+'</div><div class="cell">'+check[4]+'</div></div>');
+
+				}
+			};
+			reader.readAsText(file);
+		};
+
+
+
+		/*
+			ส่วนของการเพิ่มไฟล์ประวัติการเบิกสินค้า
+		*/
+		var tableout = $('.table-out');
+		document.getElementById('file-out').onchange = function(){
+			var file = this.files[0];
+			var reader = new FileReader();
+			reader.onload = function(progressEvent){
+			var lines = this.result.split('\n');
+			for(var line = 0; line < lines.length; line++){
+				var check = lines[line].split(",");
+
+				// tableout.append('<div class="row"><div class="cell">'+check[0]+'</div><div class="cell">'+check[1]+'</div><div class="cell">'+check[2]+'</div><div class="cell">'+check[3]+'</div><div class="cell">'+check[4]+'</div></div>');
+
+				}
 			};
 			reader.readAsText(file);
 		};
 	</script>
+
+
 </body>
 </html>

9973bdfe854f1f5200b72f873a94f3e0dba92fa2
SuttipatNew <suttipat.new@gmail.com>
2017-06-04 22:16:46 +0700

    fix delete project

--- delete_project.php
+++ delete_project.php
@@ -20,9 +20,13 @@
     }
    $sql = 'DELETE FROM project_map WHERE ProjWBS = '. $_GET['ProjWBS'];
    $result1 = $conn->query($sql);
-   $sql = 'DROP DATABASE project_'. $_GET['ProjWBS'];
+   $sql = 'DROP TABLE '. $_GET['ProjWBS'] . '__tb_inv';
    $result2 = $conn->query($sql);
-   if ($result1 && $result2) {
+   $sql = 'DROP TABLE '. $_GET['ProjWBS'] . '__tb_rec';
+   $result3 = $conn->query($sql);
+   $sql = 'DROP TABLE '. $_GET['ProjWBS'] . '__tb_out';
+   $result4 = $conn->query($sql);
+   if ($result1 && $result2 && $result3 && $result4) {
       echo "success";
    }
 ?>

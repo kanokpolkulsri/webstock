76854ff5cf23f67eeb2430499067d15b6f7a2310
SuttipatNew <suttipat.new@gmail.com>
2017-06-05 22:50:40 +0700

    add password setting file

---
+++ database_setting.php
@@ -0,0 +1,5 @@
+<?php
+    define('NORMAL_USER_PASSWORD', '');
+    define('LOGIN_PASSWORD', '');
+    define('ADMIN_USER_PASSWORD', '');
+?>
--- login.php
+++ login.php
@@ -1,11 +1,12 @@
 <?php
+	include 'database_setting.php';
 	session_start();
 	error_reporting(E_ERROR | E_PARSE);
 	date_default_timezone_set("Asia/Bangkok");
 	header('Content-Type: text/html; charset=utf-8');
 	define('DB_SERVER', 'localhost');
 	define('DB_USERNAME', 'login');
-	define('DB_PASSWORD', '1q2w3e4r');
+	define('DB_PASSWORD', LOGIN_PASSWORD);
 	define('DB_DATABASE', 'projects');
 	$conn = new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD, DB_DATABASE);
 	if($conn->connect_error) {
@@ -21,12 +22,12 @@
 		$_SESSION['name'] = $row['Name'];
 		$_SESSION['admin'] = 0;
 		$_SESSION['db_username'] = 'normal_user';
-		$_SESSION['db_password'] = '1q2w3e4r';
+		$_SESSION['db_password'] = NORMAL_USER_PASSWORD;
 		$_SESSION['last_activity'] = time();
 		if($row['State'] == 'admin') {
 			$_SESSION['admin'] = 1;
 			$_SESSION['db_username'] = 'admin_user';
-			$_SESSION['db_password'] = '1q2w3e4r';
+			$_SESSION['db_password'] = ADMIN_USER_PASSWORD;
 		}
 		header("Location: ./categorize.php");
 	} else {
